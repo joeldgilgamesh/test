3b474a41181a7ccf872ffb50a125b85d941cc147
joeldgilgamesh <joelmbompieze@gmail.com>
2020-07-07 15:37:53 +0100

    Initial version of spminfimscaa generated by JHipster-6.9.0

---
+++ src/main/java/com/spminfiscaa/config/Constants.java
@@ -0,0 +1,12 @@
+package com.spminfiscaa.config;
+
+/**
+ * Application constants.
+ */
+public final class Constants {
+
+    public static final String SYSTEM_ACCOUNT = "system";
+
+    private Constants() {
+    }
+}
---
+++ src/main/java/com/spminfiscaa/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.spminfiscaa.config.audit;
